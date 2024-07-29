4115c697b84766a34361ebc605b0f21c22131a65
JungHwan <jungwani98@ajou.ac.kr>
2024-07-29 16:00:02 +0900

    [CHORE] 랭킹페이지 중간저장

--- frontend/src/JS/Page/Ranking.js
+++ frontend/src/JS/Page/Ranking.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
+import FadeAnime from "../Component/FadeAnime";
+import TopNav from "../Component/TopNav";
+import SpringAnime from "../Component/SpringAnime";
+import "../../CSS/Ranking.css";
+import ranking from '../../Imgs/ranking.gif'
 
 const Ranking = () => {
-    return (
-        <div>
-            랭킹페이지
+  return (
+    <FadeAnime>
+      <div className="ranking-page-wrapper">
+        <div className="top-nav">
+          <TopNav />
         </div>
-    );
+
+        <div className="ranking-content-wrapper">
+          <SpringAnime from="down">
+            <div className="ranking-content">
+                <div className="icon-circle">
+                    <img alt="랭킹 아이콘" src={ranking}></img>
+                </div>
+                <h2>유저 랭킹</h2>
+                <div id="profile-card">
+                    
+                </div>
+            </div>
+          </SpringAnime>
+        </div>
+      </div>
+    </FadeAnime>
+  );
 };
 
 export default Ranking;
