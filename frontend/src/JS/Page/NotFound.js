78c001502271292d011aec13b84165713d7eaa17
JungHwan <jungwani98@ajou.ac.kr>
2024-07-29 14:48:52 +0900

    [FEAT] 탑 네비게이션 라우팅 완성

--- frontend/src/JS/Page/NotFound.js
+++ frontend/src/JS/Page/NotFound.js
@@ -4,17 +4,22 @@ import { useNavigate } from "react-router-dom";
 import image from '../../Imgs/turtleneck.png'
 import '../../CSS/NotFound.css'
 
+import FadeAnime from '../Component/FadeAnime'
+
 const NotFound = () => {
 
     const navigate = useNavigate();
 
     return (
-        <div className='notfound-wrapper'>
+        <FadeAnime>
+                    <div className='notfound-wrapper'>
             <img className='notfound-image' src={image} alt='404이미지'></img>
             <div className='notfound-title'>준비되지 않은 페이지입니다!</div>
             <div className='notfound-desc'>길을 잘못드셨네요...</div>
             <button className='notfound-button' onClick={() => navigate(-1)}>뒤로 가기</button>
         </div>
+        </FadeAnime>
+
     );
 };
 
