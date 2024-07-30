1f9004cca0d69c9219e9fc9158cf28401d0477b7
박성혁 <goldenkiwi971202@gmail.com>
2024-07-30 15:22:27 +0900

    [FIX] WebConfig에 addResourceHandler 설정

--- backend/src/main/java/com/sixstar/raidu/global/config/WebConfig.java
+++ backend/src/main/java/com/sixstar/raidu/global/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.sixstar.raidu.global.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -13,5 +14,11 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addViewController("/**/{spring:[a-zA-Z0-9-_]+}").setViewName("forward:/index.html");
         registry.addViewController("/{spring:[a-zA-Z0-9-_]+}/**").setViewName("forward:/index.html");
         registry.addViewController("/**").setViewName("forward:/index.html");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+            .addResourceLocations("classpath:/static/");
     }
 }
