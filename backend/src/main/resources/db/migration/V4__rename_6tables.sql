42a845665cf7f9611db9bfb43e37be9fb1c5387f
박성혁 <goldenkiwi971202@gmail.com>
2024-07-30 13:30:42 +0900

    [FIX] V4__rename_6tables 파일 오류 수정

--- backend/src/main/resources/db/migration/V4__rename_6tables.sql
+++ backend/src/main/resources/db/migration/V4__rename_6tables.sql
@@ -1,41 +1,16 @@
--- Check if the 'Dictionary' table exists and rename it to 'dictionary'
-IF EXISTS (SELECT * FROM information_schema.tables
-           WHERE table_schema = DATABASE() AND table_name = 'Dictionary') THEN
+-- V4__rename_6tables.sql
+
+-- Try renaming 'Dictionary' table, ignore if it fails
 ALTER TABLE `Dictionary` RENAME TO `dictionary`;
-END IF;
-
--- Check if the 'Monster' table exists and rename it to 'monster'
-IF EXISTS (SELECT * FROM information_schema.tables
-           WHERE table_schema = DATABASE() AND table_name = 'Monster') THEN
+-- Try renaming 'Monster' table, ignore if it fails
 ALTER TABLE `Monster` RENAME TO `monster`;
-END IF;
-
--- Check if the 'Region' table exists and rename it to 'region'
-IF EXISTS (SELECT * FROM information_schema.tables
-           WHERE table_schema = DATABASE() AND table_name = 'Region') THEN
+-- Try renaming 'Region' table, ignore if it fails
 ALTER TABLE `Region` RENAME TO `region`;
-END IF;
-
--- Check if the 'Report' table exists and rename it to 'report'
-IF EXISTS (SELECT * FROM information_schema.tables
-           WHERE table_schema = DATABASE() AND table_name = 'Report') THEN
+-- Try renaming 'Report' table, ignore if it fails
 ALTER TABLE `Report` RENAME TO `report`;
-END IF;
-
--- Check if the 'Room' table exists and rename it to 'room'
-IF EXISTS (SELECT * FROM information_schema.tables
-           WHERE table_schema = DATABASE() AND table_name = 'Room') THEN
+-- Try renaming 'Room' table, ignore if it fails
 ALTER TABLE `Room` RENAME TO `room`;
-END IF;
-
--- Check if the 'Season' table exists and rename it to 'season'
-IF EXISTS (SELECT * FROM information_schema.tables
-           WHERE table_schema = DATABASE() AND table_name = 'Season') THEN
+-- Try renaming 'Season' table, ignore if it fails
 ALTER TABLE `Season` RENAME TO `season`;
-END IF;
-
--- Check if the 'User' table exists and rename it to 'user'
-IF EXISTS (SELECT * FROM information_schema.tables
-           WHERE table_schema = DATABASE() AND table_name = 'User') THEN
+-- Try renaming 'User' table, ignore if it fails
 ALTER TABLE `User` RENAME TO `user`;
-END IF;
