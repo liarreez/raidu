e1fb70cab0ebf94fe1826f4303ae8cd065320a81
박성혁 <goldenkiwi971202@gmail.com>
2024-07-30 13:23:00 +0900

    [FIX] V4__rename_6tables파일 오류 수정

--- backend/src/main/resources/db/migration/V4__rename_6tables.sql
+++ backend/src/main/resources/db/migration/V4__rename_6tables.sql
@@ -1,69 +1,41 @@
--- Dictionary 테이블 이름 변경
-DO
-$$
-BEGIN
-    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = DATABASE() AND table_name = 'Dictionary') THEN
+-- Check if the 'Dictionary' table exists and rename it to 'dictionary'
+IF EXISTS (SELECT * FROM information_schema.tables
+           WHERE table_schema = DATABASE() AND table_name = 'Dictionary') THEN
 ALTER TABLE `Dictionary` RENAME TO `dictionary`;
 END IF;
-END
-$$;
 
--- Monster 테이블 이름 변경
-DO
-$$
-BEGIN
-    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = DATABASE() AND table_name = 'Monster') THEN
+-- Check if the 'Monster' table exists and rename it to 'monster'
+IF EXISTS (SELECT * FROM information_schema.tables
+           WHERE table_schema = DATABASE() AND table_name = 'Monster') THEN
 ALTER TABLE `Monster` RENAME TO `monster`;
 END IF;
-END
-$$;
 
--- Region 테이블 이름 변경
-DO
-$$
-BEGIN
-    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = DATABASE() AND table_name = 'Region') THEN
+-- Check if the 'Region' table exists and rename it to 'region'
+IF EXISTS (SELECT * FROM information_schema.tables
+           WHERE table_schema = DATABASE() AND table_name = 'Region') THEN
 ALTER TABLE `Region` RENAME TO `region`;
 END IF;
-END
-$$;
 
--- Report 테이블 이름 변경
-DO
-$$
-BEGIN
-    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = DATABASE() AND table_name = 'Report') THEN
+-- Check if the 'Report' table exists and rename it to 'report'
+IF EXISTS (SELECT * FROM information_schema.tables
+           WHERE table_schema = DATABASE() AND table_name = 'Report') THEN
 ALTER TABLE `Report` RENAME TO `report`;
 END IF;
-END
-$$;
 
--- Room 테이블 이름 변경
-DO
-$$
-BEGIN
-    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = DATABASE() AND table_name = 'Room') THEN
+-- Check if the 'Room' table exists and rename it to 'room'
+IF EXISTS (SELECT * FROM information_schema.tables
+           WHERE table_schema = DATABASE() AND table_name = 'Room') THEN
 ALTER TABLE `Room` RENAME TO `room`;
 END IF;
-END
-$$;
 
--- Season 테이블 이름 변경
-DO
-$$
-BEGIN
-    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = DATABASE() AND table_name = 'Season') THEN
+-- Check if the 'Season' table exists and rename it to 'season'
+IF EXISTS (SELECT * FROM information_schema.tables
+           WHERE table_schema = DATABASE() AND table_name = 'Season') THEN
 ALTER TABLE `Season` RENAME TO `season`;
 END IF;
-END
-$$;
 
--- User 테이블 이름 변경
-DO
-$$
-BEGIN
-    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = DATABASE() AND table_name = 'User') THEN
+-- Check if the 'User' table exists and rename it to 'user'
+IF EXISTS (SELECT * FROM information_schema.tables
+           WHERE table_schema = DATABASE() AND table_name = 'User') THEN
 ALTER TABLE `User` RENAME TO `user`;
 END IF;
-END
-$$;
