a9b8e741e7b0f0ee62abe6748047fb5f3265e340
JungHwan <jungwani98@ajou.ac.kr>
2024-08-06 15:15:22 +0900

    [FEAT] 컨셉북 페이지 초안

--- frontend/src/JS/Page/ConceptBook.js
+++ frontend/src/JS/Page/ConceptBook.js
@@ -3,10 +3,12 @@ import React from "react";
 import FadeAnime from "../Component/FadeAnime";
 import SpringAnime from "../Component/SpringAnime";
 import TopNav from "../Component/TopNav";
-import Rerenderer from "../Component/Rerenderer"
+import Rerenderer from "../Component/Rerenderer";
 // CSS 관련 import
-import '../../CSS/ConceptBook.css'
-import '../../CSS/BasicAnime.css'
+import "../../CSS/ConceptBook.css";
+import "../../CSS/BasicAnime.css";
+//
+import logo from "../../Imgs/logo.png";
 
 // ====================================================== //
 
@@ -21,9 +23,97 @@ const ConceptBook = () => {
         <div className="concept-content-wrapper">
           <SpringAnime from="down">
             <div className="concept-content">
-                <div className="focus-in-contract">
-                    1번 내용....
+              <div className="concept-logo">
+                <img src={logo} alt="로고 이미지"></img>
+              </div>
+              <div className="concept-section-wrapper">
+                {/* 첫째 문단입니다. */}
+                <div className="section-renderbox">
+                <Rerenderer>
+                  <div className="section-content">
+                    <div className="section-image focus-in-contract delay-05s">
+                      <img alt="대충 이미지"></img>
+                    </div>
+                    <div className="section-text">
+                      <div className="focus-in-contract delay-03s">이두왕국의 시작</div>
+                      <div className="focus-in-contract delay-07s">
+                        <p>대충 설명입니다.</p>
+                        <p>추후에 추가하면 됩니다.</p>
+                      </div>
+                    </div>
+                  </div>
+                </Rerenderer>
                 </div>
+
+                {/* 둘째 문단입니다. */}
+                <div className="section-renderbox">
+                <Rerenderer>
+                  <div className="section-content section-rightside">
+                    <div className="section-text">
+                      <div className="focus-in-contract delay-05s">쌓여가는 병폐</div>
+                      <div className="focus-in-contract delay-07s">
+                        <p>대충 설명입니다.</p>
+                        <p>추후에 추가하면 됩니다.</p>
+                      </div>
+                    </div>
+                    <div className="section-image focus-in-contract delay-03s">
+                      <img alt="대충 이미지"></img>
+                    </div>
+                  </div>
+                </Rerenderer>
+                </div>
+                {/* 셋째 문단입니다. */}
+                <div className="section-renderbox">
+                <Rerenderer>
+                  <div className="section-content">
+                    <div className="section-image focus-in-contract delay-05s">
+                      <img alt="대충 이미지"></img>
+                    </div>
+                    <div className="section-text">
+                      <div className="focus-in-contract delay-03s">적의 출현</div>
+                      <div className="focus-in-contract delay-07s">
+                        <p>대충 설명입니다.</p>
+                        <p>추후에 추가하면 됩니다.</p>
+                      </div>
+                    </div>
+                  </div>
+                </Rerenderer>
+                </div>
+                {/* 넷째 문단입니다. */}
+                <div className="section-renderbox">
+                <Rerenderer>
+                  <div className="section-content section-rightside">
+                    <div className="section-text">
+                      <div className="focus-in-contract delay-05s">용사의 등장</div>
+                      <div className="focus-in-contract delay-07s">
+                        <p>대충 설명입니다.</p>
+                        <p>추후에 추가하면 됩니다.</p>
+                      </div>
+                    </div>
+                    <div className="section-image focus-in-contract delay-03s">
+                      <img alt="대충 이미지"></img>
+                    </div>
+                  </div>
+                </Rerenderer>
+                </div>
+                {/* 마지막 문단입니다. */}
+                <div className="section-renderbox">
+                <Rerenderer>
+                  <div className="section-content">
+                    <div className="section-image focus-in-contract delay-05s">
+                      <img alt="대충 이미지"></img>
+                    </div>
+                    <div className="section-text">
+                      <div className="focus-in-contract delay-03s">레이두 개시!</div>
+                      <div className="focus-in-contract delay-07s">
+                        <p>대충 설명입니다.</p>
+                        <p>추후에 추가하면 됩니다.</p>
+                      </div>
+                    </div>
+                  </div>
+                </Rerenderer>
+                </div>
+              </div>
             </div>
           </SpringAnime>
         </div>
