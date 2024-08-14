f0f48dcab63954609b15ca3e62cbfc1b545acf5c
박성혁 <goldenkiwi971202@gmail.com>
2024-08-14 18:17:35 +0900

    [FIX] flyway 설정 변경

--- backend/src/main/resources/db/migration/V5__monster_description_column_add.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE monster
-ADD COLUMN description VARCHAR(255);
-
-ALTER TABLE boss_monster
-ADD COLUMN description VARCHAR(255);
-
-ALTER TABLE boss_monster
-ADD COLUMN is_mainboss BOOLEAN DEFAULT FALSE;
